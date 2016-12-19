0410d5d6edb2b686839b51b4658b1c49e7f144ff
t3knoid <t3knoid@users.noreply.github.com>
2016-12-19 13:26:38 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -86,10 +86,10 @@ def hasfiles(dir):
 def upload_folder_flat(dest, folder, names):
     """ 
     Callback function for os.path.walk function that copies each file 
-    in the given folder (folder) to the destination (dest). Uses scp 
-    method to upload files.
+    in the given folder (folder) to the destination (dest) without
+    retaining the source folder organization structure.
     """
-    if not hasfiles(folder):  # Perform operation if folder is not empty
+    if hasfiles(folder):  # Perform operation if folder contains files
         print '[INFO] Copying files in folder ' + folder + ' to ' + dest + ' started.'
         sys.stdout.flush() 
         try:
@@ -103,23 +103,27 @@ def upload_folder_flat(dest, folder, names):
             sys.stdout.flush() 
 
 def upload_app_pkg_to_docker_server(dest, folder, names):
-   # Search the list of filenames for the app server file
-   file = "".join([s for s in names if "ms-appserver".lower() in s.lower()])
-   scp(os.path.join(folder,file),dest)
+    """ 
+    This is a helper method to the os.walk.path function. It upload's the
+    application package to the given SCP destination (dest). The app archive
+    is enumerated by looking for a file that contains the string ms-appserver.
+    """
+
+    file = "".join([s for s in names if "ms-appserver".lower() in s.lower()])    # Search the list of filenames for the app server package
+    scp(os.path.join(folder,file),dest)
 
 def upload_web_pkg_to_docker_server(dest, folder, names):
-   """
-   This is a helper function to the os.walk.path function. It upload's the web 
-   archive to the given SCP destination (dest). The web archive is enumerated
-   by looking for the file 
+    """
+    This is a helper method to the os.walk.path function. It upload's the web 
+    archive to the given SCP destination (dest). The web archive is enumerated
+    by looking for the file that contains the string ms-webgui.
+    """
 
-   """
-   file = "".join([s for s in names if "ms-webgui".lower() in s.lower()])    # Search the list of filenames for the app server file
-   scp(os.path.join(folder,file),dest)
+    file = "".join([s for s in names if "ms-webgui".lower() in s.lower()])    # Search the list of filenames for the web server package
+    scp(os.path.join(folder,file),dest)
 
 def scp(file2copy,dest):
     """ Returns True or False
-
     This uses the external scp command (e.g. OPENSSH) to copy a given
     file (file2copy) to a given destination (dest). This method calls
     the helper method execute() to perform the actual execution of the
@@ -152,8 +156,7 @@ def execute(command):
     """
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    # Poll process for new output until finished
-    while True:
+    while True:     # Poll process for new output until finished
         nextline = process.stdout.readline()
         if nextline == '' and process.poll() is not None:
             break
@@ -169,8 +172,11 @@ def execute(command):
     #else:
     #    raise ProcessException(command, exitCode, output)
 
-def build_images():
-   print 'Building Docker images'
-   sys.stdout.flush() 
-   command = "ssh jenkins@" + dockerbuildserver + " '" + "./checkssh.sh" + "'"
-   execute(command)
+def build_docker_images():
+    """
+    Launches the script that builds Docker images.
+    """
+    print 'Building Docker images'
+    sys.stdout.flush() 
+    command = "ssh jenkins@" + dockerbuildserver + " '" + "./checkssh.sh" + "'"
+    execute(command)
